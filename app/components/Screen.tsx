bf73c7365cf04793d16cee3a20ef00d17024b384
Saul Maldonado <samaldonado4@gmail.com>
2020-06-10 11:29:52 -0500

    Added pull down to refresh on messagesScreen

--- app/components/Screen.tsx
+++ app/components/Screen.tsx
@@ -1,6 +1,7 @@
 import React, { ReactChildren, ReactNode } from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Constants from 'expo-constants';
+import colors from '../config/colors';
 
 const Screen = ({ children }: ScreenProps) => {
   return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
@@ -9,7 +10,7 @@ const Screen = ({ children }: ScreenProps) => {
 export default Screen;
 
 const styles = StyleSheet.create({
-  screen: { paddingTop: Constants.statusBarHeight },
+  screen: { paddingTop: Constants.statusBarHeight, flex: 1 },
 });
 
 interface ScreenProps {
--- app/screens/MessagesScreen.tsx
+++ app/screens/MessagesScreen.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList, SafeAreaView, Platform, StatusBar } from 'react-native';
 import ProfileCard from '../components/ProfileCard';
 import Screen from '../components/Screen';
 import ListItemSeparator from '../components/ListItemSeparator';
 import ListActionDeleteAction from '../components/ListActionDeleteAction';
 
-const messages = [
+const initialMessages = [
   {
     id: 1,
     title: 'T1',
@@ -20,7 +20,21 @@ const messages = [
   },
 ];
 
+interface message {
+  id: number;
+  title: string;
+  description: string;
+  image: any;
+}
+
 const MessagesScreen = () => {
+  const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleDelete = (message: message) => {
+    setMessages(messages.filter((m) => m.id !== message.id));
+  };
+
   return (
     <Screen>
       <FlatList
@@ -32,10 +46,21 @@ const MessagesScreen = () => {
             title={item.title}
             subTitle={item.description}
             onPress={() => console.log('Message Selected')}
-            renderRightActions={() => <ListActionDeleteAction onPress={() => console.log(item)} />}
+            renderRightActions={() => <ListActionDeleteAction onPress={() => handleDelete(item)} />}
           />
         )}
         ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 2,
+              title: 'T2',
+              description: 'D2',
+              image: require('../assets/mosh.jpg'),
+            },
+          ]);
+        }}
       />
     </Screen>
   );
