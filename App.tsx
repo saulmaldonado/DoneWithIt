5907da926f717a73945c17a4c90a4193211b3f40
Saul Maldonado <samaldonado4@gmail.com>
2020-06-10 02:38:23 -0500

    Added Welcome screen component, and static assest in /assests

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, Platform, StatusBar } from 'react-native';
+
+import { WelcomeScreen } from './screens/WelcomeScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <WelcomeScreen />
     </View>
   );
 }
@@ -12,8 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ screens/WelcomeScreen.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { ImageBackground, View, StyleSheet, Image, Text } from 'react-native';
+
+export const WelcomeScreen = () => {
+  const backgroundImage = require('../assets/background.jpg');
+  const logo = require('../assets/logo-red.png');
+
+  return (
+    <ImageBackground source={backgroundImage} style={styles.background}>
+      <View style={styles.logo}>
+        <Image source={logo} style={styles.logoImage} />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.buttons}>
+        <View style={styles.placeholder1} />
+        <View style={styles.placeholder2} />
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  buttons: {
+    height: '20%',
+  },
+  logoImage: {
+    height: 100,
+    width: 100,
+  },
+  logo: {
+    display: 'flex',
+    alignItems: 'center',
+    top: 100,
+  },
+  placeholder1: {
+    backgroundColor: '#fc5c65',
+    flex: 1,
+  },
+  placeholder2: {
+    backgroundColor: '#4ECDC4',
+    flex: 1,
+  },
+});
