67b7b4bffd1246447eac143ca922eae57a6b3b3c
Saul Maldonado <samaldonado4@gmail.com>
2020-06-15 23:13:59 -0500

    Change component for Messages screen navigator.

--- app/navigation/AccountNavigator.tsx
+++ app/navigation/AccountNavigator.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import MyAccountScreen from '../screens/MyAccountScreen';
+import MessagesScreen from '../screens/MessagesScreen';
 
 export type AccountNavigatorParamsList = {
   Account: undefined;
@@ -12,7 +13,7 @@ const AccountNavigator = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name='Account' component={MyAccountScreen} />
-      <Stack.Screen name='Messages' component={MyAccountScreen} />
+      <Stack.Screen name='Messages' component={MessagesScreen} />
     </Stack.Navigator>
   );
 };
