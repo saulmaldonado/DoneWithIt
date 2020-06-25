0ce82f14b648e523708f0c77e7607db0d2b359d3
Saul Maldonado <samaldonado4@gmail.com>
2020-06-25 12:48:48 -0500

    Add error view for failed network request

--- app/screens/ListingsScreen.tsx
+++ app/screens/ListingsScreen.tsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, FlatList } from 'react-native';
+
 import Screen from '../components/Screen';
 import Card from '../components/Card';
 import colors from '../config/colors';
 import { routes } from '../navigation/routes';
 import listingsApi from '../api/listings';
+import AppButton from '../components/AppButton';
 
 const ListingsScreen = ({ navigation }: any) => {
   const [listings, setListings] = useState<any>([]);
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
     loadListings();
@@ -15,11 +18,21 @@ const ListingsScreen = ({ navigation }: any) => {
 
   const loadListings = async () => {
     const response = await listingsApi.getListings();
+    if (!response.ok) {
+      return setError(true);
+    }
+    setError(false);
     setListings(response.data);
   };
 
   return (
     <Screen style={{ backgroundColor: colors.light }}>
+      {error && (
+        <>
+          <Text>Couldn't retrieve the listings.</Text>
+          <AppButton title='Retry' onPress={loadListings}></AppButton>
+        </>
+      )}
       <FlatList
         data={listings}
         renderItem={({ item }) => (
